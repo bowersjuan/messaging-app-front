e7b7e4836960ec9046af03fdba24d139c2c269ca
bowersjuan <bowersjuan@gmail.com>
2023-02-07 14:45:52 -0500

    update app with pages and routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import Show from "./Pages/Show";
+import New from "./Pages/New";
+import Edit from "./Pages/Edit";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/messages" element={<Index />} />
+          <Route path="/messages/:index" element={<Show />} />
+          <Route path="/messages/new" element={<New />} />
+          <Route path="/messages/:index/edit" element={<Edit />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
