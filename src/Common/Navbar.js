48b7914e87cd761de63725bb91c80faa8a4ae822
bowersjuan <bowersjuan@gmail.com>
2023-02-08 09:32:48 -0500

    update Navbar

--- src/Common/Navbar.js
+++ src/Common/Navbar.js
@@ -1,15 +1,27 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./Navbar.css";
 
 function Navbar() {
+  const navRef = useRef();
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
   return (
     <header>
       <h3>Logo</h3>
-      <nav>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <button></button>
+      <nav ref={navRef}>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+          <FaTimes />
+        </button>
       </nav>
-      <button></button>
+      <button className="nav-btn" onClick={showNavBar}>
+        <FaBars />
+      </button>
     </header>
   );
 }
