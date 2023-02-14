818bca758b14cfc0a8226141e4c1af8122d5a3c8
bowersjuan <bowersjuan@gmail.com>
2023-02-13 21:54:33 -0500

    update formatTimeStamp helper function

--- src/Helpers/formatters.js
+++ src/Helpers/formatters.js
@@ -2,8 +2,6 @@ function formatTimeStamp(timeStamp) {
     const dateObj = new Date(timeStamp)
     const dateArray = dateObj.toDateString().split(" ");
 
-    console.log(dateObj)
-
     return `${dateArray[1]} ${dateArray[2]}`
 }
 
