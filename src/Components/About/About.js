9d13b9226a52ae3f769815af05894a53e2a34c7a
bowersjuan <bowersjuan@gmail.com>
2023-02-16 23:26:49 -0500

    move About.js and NackgrndVideo.js into About folder

---
+++ src/Components/About/About.js
@@ -0,0 +1,11 @@
+import BackgrndVideo from "../../Assets/video.mp4";
+
+function About() {
+    return (
+        <div>
+            <BackgrndVideo />
+        </div>
+    )
+}
+
+export default About;
