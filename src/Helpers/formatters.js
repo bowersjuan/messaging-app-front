1ad78e4d32e4c8c5bbb754344db18f8aa0e50b4f
bowersjuan <bowersjuan@gmail.com>
2023-02-16 22:58:11 -0500

    update formatTimeStamp function

--- src/Helpers/formatters.js
+++ src/Helpers/formatters.js
@@ -1,8 +1,9 @@
 function formatTimeStamp(timeStamp) {
     const dateObj = new Date(timeStamp)
     const dateArray = dateObj.toDateString().split(" ");
+    const time = `${dateObj.toLocaleTimeString()}`
 
-    return `${dateArray[1]} ${dateArray[2]}`
+    return `${time} ${dateArray[1]} ${dateArray[2]}`
 }
 
 export {formatTimeStamp};
