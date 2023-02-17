74c9b3a52e4365be62b78747e22df788379edbd7
bowersjuan <bowersjuan@gmail.com>
2023-02-16 23:19:16 -0500

    update Messages and Message components for Index Page

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,15 +1,22 @@
+import { useState, useEffect } from "react";
 import {Link} from "react-router-dom"
 import { formatTimeStamp } from "../Helpers/formatters";
 import "./Message.css"
 
 function Message({message}) {
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    setUser(JSON.parse(window.localStorage.getItem("user")))
+  }, [])
+  
   return (
-    <Link to={`${message.id}`} className="message-container">
+    <Link to={`${message.user_id}`} className="message-container">
       <div className="message">
         <img alt="user" src="https://fakeface.rest/thumb/view?minimum_age=21&maximum_age=40"/>
         <div>
-          <p id="name">Juan Bowers</p>
           <p>{message.message}</p>
+          <p id="name">- {user?.name}</p>
         </div>
       </div>
       <p id="time-stamp">{formatTimeStamp(message.time_sent)}</p>
--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from "react"
+import { useParams } from "react-router-dom";
 import axios from "axios"
 import Message from "./Message";
 import "./Messages.css"
@@ -7,17 +8,18 @@ const API = process.env.REACT_APP_API_URL;
 
 function Messages() {
     const [messages, setMessages] = useState([])
+    const {id} = useParams()
 
     useEffect(() => {
-        axios.get(`${API}/messages`)
+        axios.get(`${API}/users/${id}/messages`)
         .then((response) => setMessages(response.data))
         .catch((e) => console.error(e))
     }, [])
 
     return (
         <div className="messages">
-            {messages.map((message) => {
-                return <Message key={message.id} message={message}/>
+            {messages.map((message, idx) => {
+                return <Message key={idx} message={message}/>
             })}
         </div>
     );
